fe3c81bcb39b7d42a8b29c794d157bf69fce3f4e
kitsunemonalisa <192752383+kitsunemonalisa@users.noreply.github.com>
2025-02-03 23:12:56 +0400

    update deploy config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+const [user, repo] = process.env.GITHUB_REPOSITORY.split("/");
+
 // https://astro.build/config
 export default defineConfig({
-  site: `https://${import.meta.env.GITHUB_REPOSITORY_OWNER}.github.io`,
+  site: `https://${user}.github.io`,
+  base: repo === user + ".github.io" ? undefined : `/${repo}`,
   vite: {
     plugins: [tailwindcss()],
   },
