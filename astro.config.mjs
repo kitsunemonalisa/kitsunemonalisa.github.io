862588cad9763f0f08417567a644b12586cd7abe
kitsunemonalisa <192752383+kitsunemonalisa@users.noreply.github.com>
2025-02-03 19:35:48 +0400

    add deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: `https://${import.meta.env.GITHUB_REPOSITORY_OWNER}.github.io`,
   vite: {
     plugins: [tailwindcss()],
   },
